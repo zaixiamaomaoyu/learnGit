4abc2b162f245e48e5fe85660c81fb0d15bc59d7
zhuweining <1143962543@qq.com>
2022-04-29 10:56:53 +0800

    '复习完vuex的state和getters'

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const moduleA = {
+  state: {
+    userInfo: [
+      { userId: 0, userName: 'zwn', userAge: 22 },
+      { userId: 0, userName: 'zwn', userAge: 22 }
+    ],
+    text: '好饿啊，准备上去吃饭！'
+  }
+}
+
+const getters = {
+  getUserInfoLength (state) {
+    return state.userInfo.length
+  }
+}
 export default new Vuex.Store({
   state: {
+    userInfo: [
+      { userId: 0, userName: 'zwn', userAge: 22 },
+      { userId: 0, userName: 'zwn', userAge: 22 }
+    ],
+    text: '好饿啊，准备上去吃饭！'
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  getters,
   modules: {
+    a: moduleA
   }
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{$store.state.a.userInfo}}</p>
+    <p>{{text}}</p>
+    <p>{{$store.getters.getUserInfoLength}}</p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(['userInfo', 'text'])
+  }
+}
+</script>
